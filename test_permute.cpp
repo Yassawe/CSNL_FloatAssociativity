d87ef2bed888a23dd10d5fd86a5a3da737ce70fa
Yassawe <jassaui.k@gmail.com>
2023-02-01 14:29:16 +0900

    some fixes

--- test_permute.cpp
+++ test_permute.cpp
@@ -38,7 +38,7 @@ float getSTD(vector<datatype> &sums, int R){
 
 int main(){
     int N = 8; // number of elements in test array
-    int M = 10; // how many times to repeat
+    int M = 1000; // how many times to repeat
     int R = 1000; // orders
 
     srand(0);
@@ -56,19 +56,19 @@ int main(){
             data[i] = (datatype) (rand()/(RAND_MAX-1.0)); 
         }
 
-        // //random non-repeating permutations
-        // auto rng = std::default_random_engine {};
-        // for(int r = 0; r<R; ++r){
-        //     sums[r]=makeSum(data, order, N); 
-        //     std::shuffle(std::begin(order), std::end(order), rng);
-        // }
+        //random non-repeating permutations
+        auto rng = std::default_random_engine {};
+        for(int r = 0; r<R; ++r){
+            sums[r]=makeSum(data, order, N); 
+            std::shuffle(std::begin(order), std::end(order), rng);
+        }
 
-        datatype sInOrder = makeSum(data, order, N);
-        std::reverse(order.begin(), order.end());
-        datatype sRevOrder = makeSum(data, order, N);
+        // datatype sInOrder = makeSum(data, order, N);
+        // std::reverse(order.begin(), order.end());
+        // datatype sRevOrder = makeSum(data, order, N);
 
 
-        stdvar[m] = std::abs((float)(sInOrder-sRevOrder)); 
+        // stdvar[m] = std::abs((float)(sInOrder-sRevOrder)); 
 
         stdvar[m] = getSTD(sums, R);   
     }
