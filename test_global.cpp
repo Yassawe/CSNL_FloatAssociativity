e5973e5c61f489c80e9ec3b8816d49db6f0cb554
Yassawe <jassaui.k@gmail.com>
2023-03-18 18:19:27 +0900

    main impl finished

--- test_global.cpp
+++ test_global.cpp
@@ -6,10 +6,11 @@
 #include <cmath>
 #include <iomanip>  
 #include <boost/math/distributions/laplace.hpp>
+using namespace std;
 
 
-
-
+mt19937 RNG_distro(20214229);
+mt19937 RNG_order(20214229);
 
 // bfloat16
 // typedef flx::floatx<8,7> datatype;
@@ -17,44 +18,62 @@
 // float16
 typedef flx::floatx<5,10> datatype;
 
-using namespace std;
 
-
-datatype cosineSimilarity(){
-    //TODO
-}
-
-datatype averagePairwiseCosineSimilarity(){
-    //TODO
-}
-
-
-datatype makeSum(vector<datatype>& data, vector<int>& order, int N){
-    datatype sum = 0;
-    for(int i = 0; i<N; ++i){
-        sum+=data[order[i]];
+double cosineSimilarity(const vector<datatype>& v1, const vector<datatype>& v2) {
+    double dotProduct = 0.0, norm1 = 0.0, norm2 = 0.0;
+    for (int i = 0; i < v1.size(); i++) {
+        dotProduct += v1[i] * v2[i];
+        norm1 += v1[i] * v1[i];
+        norm2 += v2[i] * v2[i];
     }
+    return dotProduct / (sqrt(norm1) * sqrt(norm2));
+}
+
+double averagePairwiseCosineSimilarity(const vector<vector<datatype>>& sums) {
+    double sumSimilarities = 0.0;
+    int count = 0;
+    for (int i = 0; i < sums.size(); i++) {
+        for (int j = i + 1; j < sums.size(); j++) {
+            double similarity = cosineSimilarity(sums[i], sums[j]);
+            sumSimilarities += similarity;
+            count++;
+        }
+    }
+    return sumSimilarities / count;
+}
+
+vector<datatype> makeSum(vector<vector<datatype>>& data, vector<int>& order, int N, int M){
+
+    vector<datatype> sum(M);
+    int idx;
+
+    for(int elem = 0; elem<M; ++elem){
+        sum[elem]=0;
+
+        for(int i = 0; i<N; ++i){
+            idx = order[i];
+            sum[elem]+=data[idx][elem];
+
+        }
+    }
+
     return sum;
 }
 
-void generateRandomNumbers(vector<vector<datatype>>& data){
+void generateRandomNumbers(vector<vector<datatype>>& data, int N, int M){
 
     float mean = 0.00000028992896587; //from ResNet50 profiling
     float std = 0.000511206; 
 
-
     //TODO: Laplace Distribution
 
     // uniform_real_distribution<datatype> dis(-1, 1);
-    normal_distribution<datatype> dis(mean, std);
+    normal_distribution<datatype> distro(mean, std);
 
-
-
-
-    // KEEP IN MIND: VALS THAT ARE OF THE SAME IDX, ARE CLOSE IN REAL ML WORKLOADS, HERE IT IS NOT THE CASE. DOES IT MATTER?
+    // KEEP IN MIND: GRADS THAT ARE OF THE SAME IDX, ARE CLOSE IN REAL ML WORKLOADS, HERE IT IS NOT THE CASE. DOES IT MATTER?
     for(int i = 0; i<N; ++i){
         for (int j = 0; j<M; ++j){
-            data[i][j] = 
+            data[i][j] = distro(RNG_distro);
         }
     }
 }
@@ -63,35 +82,19 @@ void generateRandomNumbers(vector<vector<datatype>>& data){
 void runExperiment(int N, int M, int R){
 
     vector<vector<datatype>> data(N, vector<datatype>(M));
-
-    vector<int> order(N);
-
-    vector<datatype> sums(R);
-
-    // generate N columns of M random numbers
-
-    for(int i = 0; i<N; ++i){
-        order[i] = i;
-        data[i] = (datatype) dis(gen); 
-    }
-
-    for(int r = 0; r<R; ++r){
-
-
-    }
-
-    for(int m = 0; m<M; ++m){
-        
-        
-
-        std::mt19937 rng(time(NULL));
-        for(int r = 0; r<R; ++r){
-            sums[r]=makeSum(data, order, N); 
-            shuffle(begin(order), end(order), rng);
-        }
-    }
-
+    vector<vector<datatype>> sums(R, vector<datatype> M);
     
+    vector<int> order(N);
+    for (int device = 0; device<N; ++device){
+        order[device]=device;
+    }
+
+    generateRandomNumbers(data, N, M);
+
+    for (int r = 0; r<R; ++r){
+        sums[r] = makeSum(data, order, N, M);
+        shuffle(order.begin(), order.end(), RNG_order); // probability of repeating is very low, but not 0%. ok for now.
+    }
 }
 
 
@@ -99,31 +102,14 @@ int main(){
     
     int Ns[7] = {4, 8, 16, 64, 256, 512, 1024};
     
-    int M = 1;
-    int R = 100;
+    int M = 1000;
+    int R = 10;
 
-    cout<<"BFLOAT16"<<endl;
+    cout<<"FLOAT16"<<endl;
 
     for(int i = 0; i<7; ++i){
         runExperiment(Ns[i], M, R);
     }
 
-    // runExperiment(16, 1, 24);
     return 0;
 }
-
-
-
-// float getSTD(vector<datatype> &sums, int R){
-//     float mean = 0;
-//     for(int r = 0; r<R; ++r){
-//         mean+=(float)sums[r];
-//     }
-//     mean/=R;
-//     float st = 0;
-//     for(int r = 0; r<R; ++r){
-//         st+= pow((float)(sums[r]-mean),2);
-//     }
-
-//     return sqrt(st)/sqrt(R); 
-// }
